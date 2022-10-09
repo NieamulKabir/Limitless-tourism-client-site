23794a93ac822249b9ea6d81d87b2b7e8063148c
NieamulKabir <kabir000079@gmail.com>
2022-10-09 19:30:21 +0600

    add package details

--- src/Pages/PackageDetails/PackageDetails.js
+++ src/Pages/PackageDetails/PackageDetails.js
@@ -12,18 +12,39 @@ const PackageDetails = () => {
             .then(data => setPackageDetails(data))
     }, [packageId])
 
-    
+
     return (
-        <div className=" rounded-box mx-auto">
-            <h2 className=" py-10 text-center text-4xl font-bold text-gray-700">{packageDetails?.country} </h2>
-            <img className="mx-auto rounded-box" src={packageDetails?.imgURL} alt="" />
-            <h2 className="text-gray-500 pt-10 w-5/6 lg:w-2/5 mx-auto pb-2"> {packageDetails?.description} </h2>
-            <h2 className="text-gray-700 pt-5 w-5/6 lg:w-2/5 mx-auto pb-2 text-xl font-bold">Duration: {packageDetails?.duration} Days </h2>
-            <h2 className="text-gray-700 pt-5 w-5/6 lg:w-2/5 mx-auto pb-2 text-xl font-bold">For: {packageDetails?.people} Person  </h2>
-            <h2 className="text-gray-700 pt-5 w-5/6 lg:w-2/5 mx-auto pb-2 text-xl font-bold">Ratings: <i className="fas fa-star text-yellow-500"></i>
-                {packageDetails?.ratings} </h2> 
-            <h2 className="text-gray-800 pt-5 w-5/6 lg:w-2/5 mx-auto pb-20 text-xl font-bold">Package Price: <span className="text-yellow-500 text-2xl font-bold"> {packageDetails?.price}</span> BDT </h2>
-            <NavLink to={`/booking/${packageDetails?._id}`}><button className="px-16 py-4 mb-10 bg-gray-700 rounded-full hover:bg-yellow-400 text-yellow-300 hover:text-gray-800 transition duration-300 ">Book This Service</button></NavLink>
+        <div className="font-serif">
+            <div className='w-9/12 mx-auto my-10'>
+                <div className="card lg:card-side shadow-xl bg-green-50">
+                    <figure><img className='mx-6 rounded-3xl' src={packageDetails?.imgURL} alt="Album" /></figure>
+                    <div className="card-body">
+                        <h2 className="text-center text-4xl font-bold text-violet-600">{packageDetails?.place} </h2>
+                        <h2 className="text-sm md:text-lg text-justify"> {packageDetails?.description} </h2>
+                        <h2 className="text-2xl font-bold">Duration: {packageDetails?.duration} Days </h2>
+                        <h2 className="text-xl font-bold">For: {packageDetails?.people} Person  </h2>
+                        <h2 className="">Ratings : {packageDetails?.ratings}<i className="fas fa-star text-violet-500"></i></h2>
+                        <h2 className="text-gray-800  text-xl font-bold">Package Price: <span className="text-violet-500 text-2xl font-bold"> {packageDetails?.price}</span> BDT </h2>
+                        <div className="card-actions justify-center">
+                            <NavLink to={`/booking/${packageDetails?._id}`}><button className="px-10 py-4 bg-gray-700 rounded-3xl hover:bg-violet-600 text-white transition duration-300 ">Book This Service</button></NavLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         </div>
     );
 };
